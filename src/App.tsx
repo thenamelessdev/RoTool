0afb917ec1aceb630d7e2e309bdd1baec3cb597b
thenamelessdev <aron.minarovits@gmail.com>
2026-01-22 18:58:06 +0100

    inited project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App(){
+  return(
+    <div>
+      <h2>RoTools</h2>
+      <br />
+      <p>&copy; {new Date().getFullYear()} thenamelessdev. Licensed under the MIT license.</p>
+    </div>
+  )
+}
