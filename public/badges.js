656baf079703f3c4b30e2e8207c99c4a7ac9afed
Nameless <132124594+thenamelessdev@users.noreply.github.com>
2026-01-27 16:57:56 +0100

    refactor badge system

--- public/badges.js
+++ public/badges.js
@@ -1,39 +1,25 @@
-let badges;
-let badge = [];
-
-async function getBadges() {
-    try {
+(async () => {
+    try{
         const response = await fetch("https://thenamelessdev.github.io/rotool-website/config/badges.json");
-        if (!response.ok) {
-            return console.warn("Bad response");
+        if(!response.ok){
+            return console.warn("error getting the badges - RoTool");
         }
-
-        return await response.json();
+        const json = await response.json();
+        setInterval(() => {
+            json.badges.forEach(badge => {
+                if(location.href.includes(badge.id)){
+                    const displayNameElement = document.getElementById("profile-header-title-container-name");
+                    if(!displayNameElement) return console.warn("display name element not found - RoTool");
+                    if(displayNameElement.textContent.includes(badge.text)) return;
+                    if(badge.color){
+                        displayNameElement.style.setProperty("color", badge.color, "important");
+                    }
+                    displayNameElement.textContent = `${displayNameElement.textContent} - ${badge.text}`;
+                }
+            });
+        }, 1000);
     }
     catch{
-        return console.warn("error while fetching badge config - RoTool");
+        console.warn("error getting the badges - RoTool");
     }
-}
-
-(async () => {
-    badges = await getBadges();
-    if (!badges || !badges.owners) return;
-
-    badges.owners.forEach(userId => {
-        if (location.href.includes(userId)) {
-            badge.push("Owner");
-        }
-    });
 })();
-
-setInterval(() => {
-    if(badge == []) return;
-
-    badge.forEach(badgeName => {
-        const displayNameElement = document.getElementById("profile-header-title-container-name");
-        if(!displayNameElement) return console.warn("display name element not found - RoTool");
-        if(displayNameElement.textContent.includes(badgeName)) return;
-        displayNameElement.style.color = "green";
-        displayNameElement.textContent = `${displayNameElement.textContent} - ${badgeName}`
-    });
-}, 1000);
