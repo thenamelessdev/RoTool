d2ebb993df6e4d2f555e8b301ce36a91d3b19d29
Nameless <132124594+thenamelessdev@users.noreply.github.com>
2026-02-01 22:04:43 +0100

    add quick join button and add styles for both quick join and private servers

--- public/quickJoin.js
+++ public/quickJoin.js
@@ -4,23 +4,23 @@ setInterval(() => {
     if(!gameHyperlinks) return console.warn("no game hyperlinks found");
 
     Array.from(gameHyperlinks).forEach(element => {
-        if (element.querySelector(".rotool-show-priv-servers")) return;
+        if (element.querySelector(".rotool-quick-join-btn")) return;
 
-        const showPrivateServersBtn = document.createElement("button");
+        const quickJoinBtn = document.createElement("button");
 
-        showPrivateServersBtn.className = "rotool-show-priv-servers";
+        quickJoinBtn.className = "rotool-quick-join-btn";
 
         const url = element.href;
         let parts = url.split("/");
         const gameId = parts[4];
 
-        showPrivateServersBtn.innerHTML = `<a href="/games/${gameId}#!/game-instances">Private Servers</a>`;
-        Object.assign(showPrivateServersBtn.style, {
+        quickJoinBtn.innerHTML = `<a href="roblox://placeId=${gameId}">Quick Join</a>`;
+        Object.assign(quickJoinBtn.style, {
             margin: "3px",
             borderRadius: "5px",
             border: "none"
         })
 
-        element.appendChild(showPrivateServersBtn);
+        element.appendChild(quickJoinBtn);
     });
 }, 1000);
